fae5efee39b2c1215a38aa86f641b5039dbabb20
JoaodeAlcantara <jgateixeira02@gmail.com>
2025-06-26 23:41:29 -0300

    feat: Adicionar suporte a variáveis de ambiente e limitar requisições com express-rate-limit

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    host: process.env.HOST || 3000,
-    port: process.env.PORT || 'localhost',
-}
-
-export default config
--- src/server.js
+++ src/server.js
@@ -1,19 +1,32 @@
 import express from 'express';
-import config from './config.js';
 import cors from 'cors';
 import routesPublic from './routes/public.js';
 import routesPrivate from './routes/private.js';
 import auth from './middlewares/auth.js';
+import rateLimit from 'express-rate-limit';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 100,
+    message: "Muitas requisições. Tente novamente mais tarde.",
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+
 app.use(express.json());
 app.use(cors({
     origin: '*'
 }));
-app.use('/files', express.static('./src/public/uploads'));
+app.use(limiter)
+app.use('/files', express.static(process.env.UPLOAD_PATH));
 app.use(routesPublic);
 app.use(auth.auth, routesPrivate);
 
-app.listen(config.port, config.host, () => {
-    console.log(`http://${config.host}:${config.port}`)
+app.listen(process.env.PORT, process.env.HOST, () => {
+    console.log(`http://${process.env.PORT}:${process.env.HOST}`)
 })
